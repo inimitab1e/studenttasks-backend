3a690f5cb6f45dc2d89a62570f381acac7385bbe
Aleksandr <inimitable73@gmail.com>
2022-12-05 21:35:55 +0400

    refresh rout fixed

--- src/main/kotlin/ru/studenttask/features/refresh/RefreshRemote.kt
+++ src/main/kotlin/ru/studenttask/features/refresh/RefreshRemote.kt
@@ -4,8 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class RefreshReceiveRemote(
-    val email: String,
-    val refreshToken: String
+    val email: String
 )
 
 @Serializable
--- src/main/kotlin/ru/studenttask/features/refresh/RefreshRouting.kt
+++ src/main/kotlin/ru/studenttask/features/refresh/RefreshRouting.kt
@@ -1,12 +1,10 @@
 package ru.studenttask.features.refresh
 
-import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import ru.studenttask.database.tokens.Tokens
 import ru.studenttask.secure.JWT.JwtConfig
 
 fun Application.configureRefreshRouting() {
@@ -15,20 +13,13 @@ fun Application.configureRefreshRouting() {
 
     routing {
         authenticate {
-            post("/refresh-tokens") {
+            post("/refresh") {
                 val receive = call.receive<RefreshReceiveRemote>()
-                val refreshDTO = Tokens.fetchTokens(receive.refreshToken)
 
-                if (refreshDTO == null) {
-                    call.respond(HttpStatusCode.BadRequest, "Unauthorized user!")
-                } else {
-                    val newAccessToken = jwtConfig.generateAccessToken(receive.email)
-                    val newRefreshToken = jwtConfig.generateRefreshToken(receive.email)
+                val newAccessToken = jwtConfig.generateAccessToken(receive.email)
+                val newRefreshToken = jwtConfig.generateRefreshToken(receive.email)
 
-                    Tokens.updateToken(receive.email, receive.refreshToken)
-
-                    call.respond(RefreshResponseRemote(accessToken = newAccessToken, refreshToken = newRefreshToken))
-                }
+                call.respond(RefreshResponseRemote(accessToken = newAccessToken, refreshToken = newRefreshToken))
             }
         }
     }
